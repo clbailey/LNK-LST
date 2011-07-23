c3707f8938d979c6dcdf955980e3059f86d42602
Carter Bailey <carter.l.bailey@gmail.com>
2011-07-23 17:15:40 -0500

    update to requires in lnk-lst.rb

--- lnk-lst.rb
+++ lnk-lst.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
-require 'redis'
+
+configure do
+  require 'redis'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
 
 redis = Redis.new
 
