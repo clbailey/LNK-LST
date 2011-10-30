77587dce72c3b0da388d913d1904d57cd52a71ee
Carter Bailey <carter.l.bailey@gmail.com>
2011-10-30 16:59:14 -0500

    changed css styling to Twitter bootstrap

--- lnk-lst.rb
+++ lnk-lst.rb
@@ -2,11 +2,12 @@ require 'rubygems'
 require 'sinatra'
 require 'erb'
 
-configure do
+#configure do
   require 'redis'
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+#  uri = URI.parse(ENV["REDISTOGO_URL"])
+#  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+#end
+REDIS = Redis.new
 
 helpers do
   include Rack::Utils
